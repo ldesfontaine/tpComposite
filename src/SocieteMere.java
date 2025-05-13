743bfd154fab36974848cd72b0a73892ab95c3d9
ldesfontaine <lucas.desfontaine1@gmail.com>
2025-05-13 17:31:24 +1100

    init

--- src/SocieteMere.java
+++ src/SocieteMere.java
@@ -3,15 +3,16 @@ public class SocieteMere extends Societe {
      * Calcule la somme du coût des filiales et du coûts de la societe mère
      *
      * @return la somme du coût d'entretien + le coût de la société mère
+     * for (Societe soc : filiales) {
      */
     @Override
     public String calculeCoutEntretien() {
-        StringBuilder cout = new StringBuilder();
-        for (Societe societe : getFiliales()) {
-            cout.append(societe.calculeCoutEntretien()).append("\n");
+        StringBuilder coutEntretien = new StringBuilder("Coût d'entretien de la société mère :\n");
+        for (Societe soc : filiales) {
+            coutEntretien.append(soc.calculeCoutEntretien()).append("\n");
         }
-        cout.append("Coût d'entretien de la société mère");
-        return cout.toString();
+        coutEntretien.append("Coût d'entretien de la société mère : ").append(super.calculeCoutEntretien());
+        return coutEntretien.toString();
     }
 
     /**
